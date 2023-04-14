c130dd5c797ed73722455d862a9c416ee0a9a2f9
Sunny <90709404+AlaUddin68@users.noreply.github.com>
2023-04-15 03:24:07 +0600

    Add files via upload

---
+++ Dart-Assignment-1.dart
@@ -0,0 +1,26 @@
+/*
+Name : Ala Uddin Sunny
+Assignment - 1
+Date : 15-04-2023
+Problem:
+Take a list of phone numbers example
+phoneNumber = [ +88, 01768131685, 01768171985, 01768111286, 01768131685]
+And write program that prints out the full number like ‘+8801768171985’. 
+You have to print out all numbers given in the list except +88.
+*/
+void main(){
+  List<String> phoneNumber = [
+    "+88",
+    "01768131685",
+    "01768171985",
+    "01768111286",
+    "01768131685",
+    "01768151685",
+    "01700311185",
+
+  ];
+  for (int i = 1; i < phoneNumber.length; i++){
+    print("'" + phoneNumber[0] + phoneNumber[i] + "'");
+
+  }
+}
